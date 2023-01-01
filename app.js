43f36d5e8cd29532e87565269bcd9614a2100fb5
Александр Морев <a.morev@kryptonite.ru>
2023-01-02 02:29:25 +0300

    Add loggers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const routes = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/loggers');
 
 const { DB_URI } = require('./constants/db');
 const { DEFAULT_PORT } = require('./constants/conn');
@@ -18,7 +19,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.use(routes);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(require('./middlewares/error-handling'));
